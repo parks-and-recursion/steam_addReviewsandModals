ee458b858679de65a2842119238545568b720d15
Joyce Ma <pikajoyce@gmail.com>
2019-05-29 15:06:59 -0700

    Fixed drop down tables

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AddReview from './components/App.jsx';
 
-ReactDOM.render(<AddReview />, document.getElementById('app'));
+ReactDOM.render(<AddReview />, document.getElementById('addReview'));
